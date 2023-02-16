1918872795e8b499e15a184e70b4f19afb2f51ba
Léo Raiz <leonardoraiz@icloud.com>
2023-02-15 22:11:17 -0300

    Aula 01

--- Aula_01/index.js
+++ Aula_01/index.js
@@ -3,4 +3,16 @@ const express = require("express");
 //Clean Code
 const app = express();
 //Chamar a porta do servidor
-app.listen(8080);
+app.listen(8080, () => {
+  console.log("O servidor está ativo na porta 8080!!!");
+});
+
+//Criando a primeira rota
+app.get("/", () => {
+  console.log("Opa, deu certo o acesso!!!");
+});
+let nome = "Léo"
+//Rota de envio de uma marcação
+app.get("/testeHTML", (req, res) => {
+  res.send("<h1>Olá Minha primeira página</h1>" + nome);
+});
